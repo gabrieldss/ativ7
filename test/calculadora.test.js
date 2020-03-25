1d2eb00156cb3fade56543bb6a78e3f50b7ef1ac
gabrieldss <gabriel-santosouza@hotmail.com>
2020-03-25 18:05:35 -0300

    ajustes e teste DBA

--- test/calculadora.test.js
+++ test/calculadora.test.js
@@ -7,28 +7,53 @@ let calculadora = new Calculadora();
 
 
 describe('Calculadora', () => {
-    test('Desenvolvedor e ganha mais de 3000', () => {
-        let desenvolvedor = calculadora.montarSalario({
+    test('Desenvolvedor e ganha 3000 ou mais', () => {
+        let func = calculadora.montarSalario({
             nome: 'Joao da Silva',
             email: 'joao@gmail.com',
             salario_base: 3500,
             cargo: 'Desenvolvedor'
         });
-        expect(desenvolvedor.length).toBe(1);
-        expect(desenvolvedor[0]).toBeDeepCloseTo({
+        expect(func.length).toBe(1);
+        expect(func[0]).toBeDeepCloseTo({
             salario_final: 2800
         }, 2);
     });
     test('Desenvolvedor e ganha menos de 3000', () => {
-        let desenvolvedor = calculadora.montarSalario({
+        let func = calculadora.montarSalario({
             nome: 'Carlos da Silva',
             email: 'carlos@gmail.com',
             salario_base: 2500,
             cargo: 'Desenvolvedor'
         });
-        expect(desenvolvedor.length).toBe(1);
-        expect(desenvolvedor[0]).toBeDeepCloseTo({
+        expect(func.length).toBe(1);
+        expect(func[0]).toBeDeepCloseTo({
             salario_final: 2250
+        }, 2);
+    });
+
+    test('DBA e ganha 2000 ou mais', () => {
+        let func = calculadora.montarSalario({
+            nome: 'Fernando da Silva',
+            email: 'fernando@gmail.com',
+            salario_base: 2200,
+            cargo: 'DBA'
+        });
+        expect(func.length).toBe(1);
+        expect(func[0]).toBeDeepCloseTo({
+            salario_final: 1650
+        }, 2);
+    });
+    test('DBA e ganha menos de 2000', () => {
+        let func = calculadora.montarSalario({
+            nome: 'Lúcia da Silva',
+            email: 'lucia@gmail.com',
+            salario_base: 1800,
+            cargo: 'DBA'
+        });
+        expect(func.length).toBe(1);
+        expect(func[0]).toBeDeepCloseTo({
+            salario_final: 1530
         }, 2);
     });
 });
